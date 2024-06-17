ea8d7ae33944ca679f73dc3d25c932f4ae90d082
DavidBusto <125014973+DavidBusto@users.noreply.github.com>
2024-06-17 22:40:24 +0200

    Delete textil directory

--- textil/controllers/controlHistorial.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../models/modelhistorial.php';
-
-function mostrarhistorial() {
-    $operaciones = obtenerhistorial();
-    require '../views/historial.php';
-}
-mostrarhistorial();
--- textil/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once './controllers/controlLogin.php';
-if (isset($_POST['submit'])) {
-   loginUser($_POST['usuario'],$_POST['password']);
-}
-
-include_once './views/login.php';
-?>
